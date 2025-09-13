e71f0d4070e4f253f12fc1eb34e91ec314c490df
KushDastane <kushdastane777@gmail.com>
2025-09-13 21:43:30 +0530

    Checking whats wrong with last PR

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Register from './pages/Register.jsx'
-import Login from './pages/Login.jsx'
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes/router.jsx';
+import React from "react";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router.jsx";
 
 function App() {
-
-    return (
-        <>
-            <RouterProvider router={router} />
-    
-        </>
-    )
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
-export default App
+export default App;
