0d46690c04f3eceae4565d56a85ba0c0764573e7
KushDastane <kushdastane777@gmail.com>
2025-09-14 12:00:58 +0530

    Added Protected Routes

--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -3,15 +3,30 @@ import { createBrowserRouter } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import Landing from "../pages/Landing"; //Added by Arya
+import Landing from "../pages/Landing";
 import Dashboard from "../pages/Dashboard";
 import AlumniDirectoryUI from "../pages/AlumniDirectory";
+import ProtectedRoute from "../pages/ProtectedRoute";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Landing /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
-  { path: "/dashboard", element: <Dashboard /> },
-  { path: "/aldir", element: <AlumniDirectoryUI /> },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+        <Dashboard />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/aldir",
+    element: (
+      <ProtectedRoute>
+        <AlumniDirectoryUI />
+      </ProtectedRoute>
+    ),
+  },
   { path: "*", element: <Register /> },
 ]);
