2df7d6af40d112e8e4447fa876c4b92a78d382a4
Divit <divit26j@gmail.com>
2026-09-09 06:19:43 +0000

    fix

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`MongoDB connected`)
-    } catch (error) {
-        console.log("MongoDB connection error", error)
+    if (!process.env.MONGODB_URI) {
+        throw new Error("MONGODB_URI is not set. Create server/.env from server/.env.example")
     }
+
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log(`MongoDB connected`)
 }
 
 export default connectDB
--- server/src/index.js
+++ server/src/index.js
@@ -11,5 +11,5 @@ connectDB()
     })
     .catch((err) => {
         console.log(`MongoDB connection error`, err)
+        process.exit(1)
     })
-
